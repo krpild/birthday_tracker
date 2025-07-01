8c2a8be64ce7f8dedbbca1b0ab446b68498c3222
krpild <kristjan.pilden@gmail.com>
2025-07-01 22:20:14 +0300

    Basic bot added for testing purposes

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"We are ready to go in, {bot.user.name}")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
