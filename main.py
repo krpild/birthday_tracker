7d8776f7c1ea949bcb0a331b7eb8a0ade319dd1a
krpild <kristjan.pilden@gmail.com>
2025-07-02 18:00:09 +0300

    Added a check for existing names to avoid duplicate entries

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import logging
 from discord.ext import tasks, commands
 from discord import app_commands
 from dotenv import load_dotenv
-import json
 import requests
 import os
 import datetime
@@ -15,8 +14,14 @@ channel_id = os.getenv('CHANNEL_ID')
 db_domain = os.getenv('DB_DOMAIN')
 db_token = os.getenv('DB_DOMAIN_TOKEN')
 
+headers = {
+        "apikey": db_token,
+        "Authorization": f"Bearer {db_token}",
+        "Content-Type": "application/json"
+    }
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
@@ -28,19 +33,12 @@ bot = commands.Bot(command_prefix="/", intents=intents)
 async def on_ready():
     await bot.tree.sync()
     print(f"Bot is ready. Logged in as {bot.user}")
-    check_date_change.start()
+    notify_birthday.start()
 
 
-@bot.tree.command(name="show_people", description="Displays people in system")
-async def show_people(interaction: discord.Interaction):
-
-    url = db_domain
-    headers = {
-        "apikey": db_token,
-        "Authorization": f"Bearer {db_token}",
-        "Content-Type": "application/json"
-    }
-    response = requests.get(url, headers=headers)
+@bot.tree.command(name="show_birthdays", description="Displays all registered birthdays")
+async def show_birthdays(interaction: discord.Interaction):
+    response = requests.get(db_domain, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
@@ -55,7 +53,7 @@ async def show_people(interaction: discord.Interaction):
         for person in sorted_birthdays:
             result += "\n" + person.name + " : " + person.birthday.strftime('%B %d')
 
-        await interaction.response.send_message("Here are all the people:" + result)
+        await interaction.response.send_message("Here are all the registered birthdays:" + result)
 
     else:
         await interaction.response.send_message("Could not access database domain.")
@@ -64,16 +62,15 @@ async def show_people(interaction: discord.Interaction):
 @bot.tree.command(name="upload_birthday", description="Upload a birthday.")
 @app_commands.describe(name="Global user name, not the display name.", date="Birth date of the person. Use YYYY-MM-DD format.")
 async def upload_birthday(interaction: discord.Interaction, name: str, date: str):
-    url = db_domain
-    headers = {
-        "apikey": db_token,
-        "Authorization": f"Bearer {db_token}",
-        "Content-Type": "application/json"
-    }
     try:
-        datetime.date.fromisoformat(date)
+        date = datetime.date.fromisoformat(date)
+        print(date)
         
-        response = requests.post(url,headers=headers, json={'name': name, 'birthday': date})
+        if user_exists(name):
+            await interaction.response.send_message("Please refrain from adding duplicate users.")
+        
+
+        response = requests.post(db_domain, headers=headers, json={'name': name, 'birthday': date})
 
         if response.status_code == 201:
             await interaction.response.send_message(f"{name} added to birthday list.")
@@ -84,23 +81,18 @@ async def upload_birthday(interaction: discord.Interaction, name: str, date: str
 
 
 @tasks.loop(seconds=30)
-async def check_date_change():
+async def notify_birthday():
     channel = bot.get_channel(int(channel_id))
     now = datetime.date.today()
-    url = db_domain
-    headers = {
-        "apikey": db_token,
-        "Authorization": f"Bearer {db_token}",
-        "Content-Type": "application/json"
-    }
-    response = requests.get(url, headers=headers)
+    
+    response = requests.get(db_domain, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
         
         people = []
         for entry in data:
-            people.append(Person(entry['name'], datetime.date.fromisoformat(entry['birthday'])))
+            people.append(Person(entry['name'].lower(), datetime.date.fromisoformat(entry['birthday'])))
     
     for person in people:
         user = discord.utils.find(lambda m: m.name == person.name, channel.guild.members)
@@ -113,3 +105,16 @@ async def check_date_change():
 
 
 bot.run(discord_token, log_handler=handler, log_level=logging.DEBUG)
+
+def user_exists(name: str):
+    response = requests.get(db_domain, headers=headers)
+    people = []
+    if response.status_code == 200:
+        data = response.json()
+        for entry in data:
+            people.append(Person(entry['name'], datetime.date.fromisoformat(entry['birthday'])))
+        for person in people:
+            if person.name == name:
+                return True
+    return False
+    
